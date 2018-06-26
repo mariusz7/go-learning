e76a4253d6b29f252dc810805acd15118bff091b
Mariusz Kędzierawski <39653122+mariusz7@users.noreply.github.com>
2018-06-26 12:07:10 +0200

    First version passing unit tests

--- HashMap/MyHashMap.go
+++ HashMap/MyHashMap.go
@@ -19,10 +19,10 @@ type KeyValue struct {
 }
 
 func (hm *MyHashMap) Insert(key int, value string) {
-	var hash int = keyToHash(key)
-	var index int = hashToIndex(hash)
+	var hash uint32 = hm.keyToHash(key)
+	var index uint32 = hm.hashToIndex(hash)
 
-	hm.array[index] = append(hm.array[index], KeyValue{key: key, value; value})
+	hm.array[index] = append(hm.array[index], KeyValue{key: key, value: value})
 }
 
 //Function from https://stackoverflow.com/questions/13582519/how-to-generate-hash-number-of-a-string-in-go
@@ -34,22 +34,24 @@ func hash(s string) uint32 {
 
 func (hm *MyHashMap) keyToHash(key int) uint32 {
 
+	//Hmm, I am sure there is a better way to directly
+	//calculate hash from uint32...
 	var keyString string = strconv.Itoa(key)
 	var hash uint32 = hash(keyString)
 
 	return hash
 }
 
-func (hm *MyHashMap) hashToIndex(hash int) int {
-	return hash % len(hm.array)
+func (hm *MyHashMap) hashToIndex(hash uint32) uint32 {
+	return hash % uint32(len(hm.array))
 }
 
 func (hm *MyHashMap) GetValue(key int) (string, error) {
 
-	var hash int = keyToHash(key)
-	var index int = hashToIndex(hash)
+	var hash uint32 = hm.keyToHash(key)
+	var index uint32 = hm.hashToIndex(hash)
 
-	for _, pair := range hm.array {
+	for _, pair := range hm.array[index] {
 		if key == pair.key {
 			return pair.value, nil
 		}
